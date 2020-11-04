3424c559400ff48f2eb60bc0733bb11d8ba9df65
Crowdbotics <team@crowdbotics.com>
2020-11-04 20:18:05 +0000

    Initial commit.

---
+++ ios/social_lync_22306Tests/social_lync_22306Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface social_lync_22306 : XCTestCase
+
+@end
+
+@implementation social_lync_22306
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
